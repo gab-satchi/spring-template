f1994e4217dca6150a0b24753ff04d2a4b5ae902
Richard Begley <rbegley@pivotal.io>
2017-05-29 11:12:27 -0400

    Initial commit

---
+++ src/main/java/com/pivotal/springtemplate/SpringTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.pivotal.springtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateApplication.class, args);
+	}
+}
